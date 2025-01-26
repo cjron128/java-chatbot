ec5c9c95488d3185815505afa9958c8b04722360
Chris Ron <ronc@wwu.edu>
2025-01-26 02:08:41 -0800

    added postgresql backend logic

--- src/main/java/com/cjron128/chatbot/config/Config.java
+++ src/main/java/com/cjron128/chatbot/config/Config.java
@@ -1,9 +1,13 @@
 package com.cjron128.chatbot.config;
 
+import java.util.Properties;
 import java.util.logging.*;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+
+
+import java.sql.*;
 
 public class Config {
 
@@ -28,5 +32,19 @@ public class Config {
 
     // Public static variables to access configuration values
     public static final String API_KEY = configFile.getString("OPENAI_API_KEY");
+
+    // Database connection values
+    public static final String DB_URL = configFile.getString("DB_URL");
+    public static final String DB_USER = configFile.getString("DB_USER");
+    public static final String DB_PASSWORD = configFile.getString("DB_PASSWORD");
+
+
+    public static Connection getConnection() throws SQLException {
+        Properties props = new Properties();
+        props.setProperty("user", DB_USER);
+        props.setProperty("password", DB_PASSWORD);
+
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
 
 }
